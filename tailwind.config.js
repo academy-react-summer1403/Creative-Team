e3f636f9a57ac1916994531fd9b1ad2887b64372
maei <mdh.digital@gmail.com>
2024-10-04 22:36:25 -0700

    Update tailwind configuration & styles

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFmily: {
+        body: ["nunito"],
+        vazir: ["Vazir"],
+      },
+    },
+  },
+  plugins: [],
+};
