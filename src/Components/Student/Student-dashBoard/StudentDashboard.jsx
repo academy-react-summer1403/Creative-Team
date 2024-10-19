a7323bd4278f3a1196129c12d19c4c6be91351e0
maei <mdh.digital@gmail.com>
2024-10-19 00:13:39 -0700

    add panel

---
+++ src/Components/Student/Student-dashBoard/StudentDashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const StudentDashboard = () => {
+  return (
+    <div className="w-[1440px] h-[1024px] bg-black text-white flex flex-col">
+      <div className=" h-[84px] border border-white">1</div>
+      <div className="flex border h-[940px] border-white">
+        <div className="border border-white">2</div>
+        <div className="border border-white">3</div>
+      </div>
+    </div>
+  );
+};
+
+export default StudentDashboard;
