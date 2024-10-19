4899578dc1b5eae67a244817d39fc007e5350081
maei <mdh.digital@gmail.com>
2024-10-18 23:03:58 -0700

    edit branch to student panel dashboard

---
+++ src/Components/Student/Student-dashBoard/StudentDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StudentDashboard = () => {
+  return (
+    <div className="">
+      <div className=""></div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default StudentDashboard;
