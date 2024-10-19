b0d9dc7e17db2a68d0f393f3165e3222a46e94f5
maei <mdh.digital@gmail.com>
2024-10-18 21:57:26 -0700

    change it

--- src/Components/Common/Header/index.jsx
+++ src/Components/Common/Header/index.jsx
@@ -1,77 +1,55 @@
-// import React from "react";
-// import HeaderItems from "./HeaderItems";
-// import { PiBellThin } from "react-icons/pi";
-// import DarkModeToggle from "../DarkMode";
-// import { MenuWithToggle } from "./HeaderItems/MenuToggle";
-// import { Link } from "react-router-dom";
+import React from "react";
 
-// const Header = () => {
-//   return (
-//     <header className="bg-white dark:bg-indigo-950  p-4 ">
-//       <div className="container mx-auto flex justify-between items-center">
-//         <div className="flex items-center space-x-4">
-//           <img
-//             src="https://s3-alpha-sig.figma.com/img/8978/2e65/1551396a2baebe523401d4b33d0ab02a?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KQ~dcIIM~JdW9LpmW5k993TDj8dn8ZkN1Qtttuy0-aSHkPk1sIyxi2ojMCsPG8vcb2~g8uXnxbCULxQJadpQ2JwI7Dwwxpp4Wm-8hhU8GpNvno0ez-2K9VQGnYe~CrZ98sZ5XUYXmAXY7jDzgH7HPYtrW9G5L7F~9KM9PKI~bFmNq5CdwiOSQV5h5afOc2RATwQZFEMpzuDfiLxh2Jo5A-IJcbTalKpbonbw~-ujm7ivgBYsdWTqgrvF6PRGappVjgHwdWZlhcLcfWHliNwBwEGJrafUpFwxFPrl1S6XiWrCc6uvAP8O7OVF9rTsLeuddKBVEjlCZKSpz8HWAImLkw__"
-//             alt=""
-//             className="size-10"
-//           />
-//           <h1 className="hidden sm:block text-xl font-bold text-cyan-500">
-//             آکادمی کد نویسی بحر
-//           </h1>
-//         </div>
-//         <div className="hidden lg:flex flex-nowrap items-center ml-10 rounded-full bg-black dark:bg-white p-1 pr-4">
-//           <nav>
-//             <ul className="flex  justify-between text-white dark:text-black w-72">
-//               <HeaderItems Item="خانه" link="/" />
-//               <HeaderItems Item="دوره ها" link="/courses" />
+const Header = () => {
+  return (
+    <>
+      <header className=" flex flex-row justify-between gap-64 my-3 mx-12">
+        <div className="hidden mobile:flex flex-row gap-1 w-24 h-10 mt-1">
+          <div className="bg-black w-10 h-10 border-black rounded-3xl cursor-pointer ">
+            <img className="my-2 mx-2" src="src/assets/HeaderImg/moon-02.png" />
+          </div>
+          <div className="w-10 h-10 bg-white border-gray-700 rounded-3xl cursor-pointer">
+            <img className="my-2 mx-2" src="/imgLand/notification-02.png" />
+          </div>
+        </div>
+        <div className="hidden mobile:flex flex-row gap-8 relative left-10 top-0 py-1 pl-1 pr-1 h-14 w-1/2 items-center bg-black rounded-full">
+          <bottom className=" text-center bg-blue-500 rounded-3xl cursor-pointer laptop:text-lg laptop:px-3 py-2 px-0 mobile:text-sm text-xs">
+            ثبت نام یاورود
+          </bottom>
 
-//               <HeaderItems Item="بلاگ ها" />
-//               <HeaderItems Item="درباره ما" />
-//             </ul>
-//           </nav>
-//           <Link to={"/signup"} className="space-x-2">
-//             <button className="bg-blue-600 text-white py-2 px-4 rounded-full mr-10">
-//               ثبت نام یا ورود
-//             </button>
-//           </Link>
-//         </div>
-//         <div className="flex">
-//           <div className="flex lg:hidden items-center space-x-2 p-2 rounded-full border border-gray-300 w-15 h-11">
-//             {/* Down Arrow Icon */}
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               className="h-5 w-5 text-gray-500"
-//               viewBox="0 0 20 20"
-//               fill="currentColor"
-//             >
-//               <path
-//                 fillRule="evenodd"
-//                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-//                 clipRule="evenodd"
-//               />
-//             </svg>
-//             {/* Avatar */}
-//             <div className="size-8 rounded-full overflow-hidden">
-//               <img
-//                 src="" // Replace with actual image URL or use a local image
-//                 alt=""
-//                 className="w-full h-full object-cover"
-//               />
-//             </div>
-//           </div>
-//           <div className="items-center hidden lg:flex size-11 justify-center border-solid border-black bg-slate-100 rounded-full">
-//             <PiBellThin />
-//           </div>
-//           <div className="hidden items-center lg:flex size-11 justify-center bg-black  dark:bg-white text-white dark:text-black rounded-full">
-//             <DarkModeToggle />
-//           </div>
-//           <div className="items-center  lg:hidden flex size-11 justify-center bg-black dark:bg-white mr-2 text-white dark:text-black rounded-full">
-//             <MenuWithToggle />
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
+          <div className="flex flex-row items-center gap-10 p-0 font-serif not-italic font-medium text-base leading-5 text-white cursor-pointer">
+            <div>درباره ما</div>
+            <div>بلاگ ها</div>
+            <div>دوره ها</div>
+            <div>خانه</div>
+          </div>
+        </div>
 
-// export default Header;
+        <div className=" hidden tablet:flex flex-row items-center space-x-2 mt-1">
+          <img className="inline mt-2" src="\imgLand\Untitled-1 3.svg" />
+          <img className="" src="\imgLand\Untitled-1 4.svg" />
+        </div>
+      </header>
+      {/******* Resp Mobile *** */}
+      <div className=" border border-gray-50 p-3 m-3 flex flex-row justify-between gap-5 mobile:hidden">
+        <div className=" flex flex-row justify-between gap-3">
+          <img src="\imgLand\Group 20.png" />
+          <div className=" w-28 border border-gray-300 rounded-full flex flex-row justify-center gap-3">
+            <div>
+              <img
+                className="rounded-full w-4/5 mt-2"
+                src="\imgLand\Peppe – 07.png"
+              />
+            </div>
+            <div>
+              <img className="mt-4" src="\imgLand\arrow-down-01.png" />
+            </div>
+          </div>
+        </div>
+        <img className="" src="\imgLand\Untitled-1 4.svg" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
