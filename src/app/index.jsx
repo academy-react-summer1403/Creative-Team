a5cbb6aceef51babffbe2b58bd7e473f5c8a01e2
maei <mdh.digital@gmail.com>
2024-10-20 08:14:49 -0700

    Common Structure Student Pannel for all

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,24 +2,21 @@ import React from "react";
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "./vite.svg";
-import FirstPageForgot from "../Screen/Forgot/PageForgot01/index.jsx";
+// import FirstPageForgot from "../Screen/Forgot/PageForgot01/index.jsx";
 import "./App.css";
 // import { authRoutes } from "../Config/Router/Auth.router";
 import StudentDashboard from "../Components/Student/Student-dashBoard/StudentDashboard";
-
+// import CommonStructurePanel from "../Components/Student/Student-dashBoard/CommonStructurePanel/commonStructurePanel.jsx";
 function App() {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
-<<<<<<< HEAD
       {" "}
       {/* Routers */}
       <StudentDashboard />
-=======
-      Hello React
-      <FirstPageForgot />
+      {/* <CommonStructurePanel /> */}
+      {/* <FirstPageForgot /> */}
       {/* Routers */}
->>>>>>> main
     </>
   );
 
