d532609208ab9fb403228c67e119b8c9877e1206
faeghe444 <faeghe.najafzadeh@gmail.com>
2024-11-19 20:29:25 +0330

    filtercourse

--- src/Components/Courses/Course/CategoryTabs/index.jsx
+++ src/Components/Courses/Course/CategoryTabs/index.jsx
@@ -1,26 +1,76 @@
 import React from "react";
-
+import { useState } from "react";
 const index = () => {
+  const [activeCategoryTab, setActiveCategoryTab] = useState("جدیدترین");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCategoryTab = (category) => {
+    setActiveCategoryTab(category);
+    setIsModalOpen(false);
+  };
   return (
     <>
-      <div className=" hidden mobile:block mobile:flex flex-row justify-end mr-10 mt-4 font-serif text-center font-semibold text-black dark:text-white">
-        <ul className="py-2">
-          <li className="inline border border-gray-600 rounded-3xl mx-2 py-1 px-2 font-serif text-center font-semibold">
-            ارزان ترین
-          </li>
-          <li className="inline border border-gray-600 rounded-3xl mx-2 py-1 px-2 font-serif text-center font-semibold">
-            گران ترین
-          </li>
-          <li className="inline border border-gray-600 rounded-3xl mx-3 py-1 px-2 font-serif text-center font-semibold">
-            محبوب ترین
-          </li>
-          <li className="inline border border-red-600 rounded-3xl mr-4 py-1 px-3 text-red-600 font-serif text-center font-semibold">
-            جدید ترین
-          </li>
-          <li className="inline  text-black font-serif text-center font-bold text-xl dark:text-white">
-            ترتیب{" "}
-          </li>
-        </ul>
+      <div className=" hidden mobile:block mobile:flex flex-row-reverse justify-start gap-1 mr-10 mt-4 mb-4 font-serif text-center font-semibold text-black dark:text-white">
+        <p className="  text-black text-center font-bold text-xl mt-2 ml-3 dark:text-white">
+          ترتیب{" "}
+        </p>
+
+        <button
+          onClick={() => handleCategoryTab("جدیدترین")}
+          className={`px-4 py-2 h-10 rounded-full border border-gray-300 ${
+            activeCategoryTab == "جدیدترین"
+              ? "bg-red-600 text-white"
+              : "bg-white border-gray-400 text-gray-800 hover:border-red-500 hover:text-red-500"
+          }`}
+        >
+          جدیدترین
+        </button>
+
+        <button
+          onClick={() => handleCategoryTab("محبوب ترین")}
+          className={`px-4 py-2 h-10 rounded-full border border-gray-300 ${
+            activeCategoryTab == "محبوب ترین"
+              ? "bg-red-600 text-white"
+              : "bg-white border-gray-400 text-gray-800 hover:border-red-500 hover:text-red-500"
+          }`}
+        >
+          محبوب ترین
+        </button>
+
+        <button
+          onClick={() => handleCategoryTab("گران‌ترین")}
+          className={`px-4 py-2 h-10 rounded-full border border-gray-300 ${
+            activeCategoryTab == "گران‌ترین"
+              ? "bg-red-600 text-white"
+              : "bg-white border-gray-400 text-gray-800 hover:border-red-500 hover:text-red-500"
+          }`}
+        >
+          گران‌ترین
+        </button>
+
+        <button
+          onClick={() => handleCategoryTab("ارزان‌ترین")}
+          className={`px-4 py-2 h-10 rounded-full border border-gray-300 ${
+            activeCategoryTab == "ارزان‌ترین"
+              ? "bg-red-600 text-white"
+              : "bg-white border-gray-400 text-gray-800 hover:border-red-500 hover:text-red-500"
+          }`}
+        >
+          ارزان‌ترین
+        </button>
+
+        {/* <li className="inline border border-gray-600 rounded-3xl mx-2 py-1 px-2 font-serif text-center font-semibold">
+          ارزان ترین
+        </li>
+        <li className="inline border border-gray-600 rounded-3xl mx-2 py-1 px-2 font-serif text-center font-semibold">
+          گران ترین
+        </li>
+        <li className="inline border border-gray-600 rounded-3xl mx-3 py-1 px-2 font-serif text-center font-semibold">
+          محبوب ترین
+        </li>
+        <li className="inline border border-red-600 rounded-3xl mr-4 py-1 px-3 text-red-600 font-serif text-center font-semibold">
+          جدید ترین
+        </li> */}
       </div>
     </>
   );
